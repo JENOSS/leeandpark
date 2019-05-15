3a79607d3e306fdfc422354de6c2341c84f75bec
JENOSS <nari1539@gmail.com>
2019-05-15 20:57:19 +0900

    commit 1

---
+++ src/main/java/com/park/webservice/controller/MyController.java
@@ -0,0 +1,49 @@
+package com.park.webservice.controller;
+
+import com.park.webservice.model.User;
+import com.park.webservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MyController {
+
+
+    @Autowired
+    UserRepository repository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView welcome (ModelAndView mav){
+        mav.setViewName("welcome");
+        mav.addObject("msg","this is sample content");
+        return mav;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login (ModelAndView mav){
+        mav.setViewName("login");
+        mav.addObject("msg","this is sample content");
+        return mav;
+    }
+
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView register(@ModelAttribute("formModel") User user, ModelAndView mav){
+        mav.setViewName("register");
+        mav.addObject("msg","this is sample content");
+        return mav;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView regiseter_post(@ModelAttribute("formModel") User user, ModelAndView mav){
+        repository.saveAndFlush(user);
+        return new ModelAndView("redirect:/");
+    }
+
+
+}
---
+++ src/main/java/com/park/webservice/model/User.java
@@ -0,0 +1,57 @@
+package com.park.webservice.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "member")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
